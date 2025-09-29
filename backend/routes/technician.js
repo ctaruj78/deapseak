1813ba59cea5ac3001600d1391a9b3983ce7ec05
ctaruj78 <ctaruj78@gmail.com>
2025-09-29 08:10:23 +0000

    new

--- backend/routes/technician.js
+++ backend/routes/technician.js
@@ -1,17 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/technicianController');
+const { body } = require('express-validator');
 
 // MongoDB routes
 router.get('/mongo', controller.getAllTechniciansMongo);
-router.post('/mongo', controller.createTechnicianMongo);
-router.put('/mongo/:id', controller.updateTechnicianMongo);
+router.post('/mongo',
+	[
+		body('firstName').isString().notEmpty(),
+		body('lastName').isString().notEmpty(),
+		body('status').isString().notEmpty(),
+		body('battery').isInt({ min: 0, max: 100 }),
+		body('signal').isInt({ min: 0, max: 100 })
+	],
+	controller.createTechnicianMongo
+);
+router.put('/mongo/:id',
+	[
+		body('firstName').optional().isString(),
+		body('lastName').optional().isString(),
+		body('status').optional().isString(),
+		body('battery').optional().isInt({ min: 0, max: 100 }),
+		body('signal').optional().isInt({ min: 0, max: 100 })
+	],
+	controller.updateTechnicianMongo
+);
 router.delete('/mongo/:id', controller.deleteTechnicianMongo);
 
 // PostgreSQL routes
 router.get('/sql', controller.getAllTechniciansSQL);
-router.post('/sql', controller.createTechnicianSQL);
-router.put('/sql/:id', controller.updateTechnicianSQL);
+router.post('/sql',
+	[
+		body('firstName').isString().notEmpty(),
+		body('lastName').isString().notEmpty(),
+		body('status').isString().notEmpty(),
+		body('battery').isInt({ min: 0, max: 100 }),
+		body('signal').isInt({ min: 0, max: 100 })
+	],
+	controller.createTechnicianSQL
+);
+router.put('/sql/:id',
+	[
+		body('firstName').optional().isString(),
+		body('lastName').optional().isString(),
+		body('status').optional().isString(),
+		body('battery').optional().isInt({ min: 0, max: 100 }),
+		body('signal').optional().isInt({ min: 0, max: 100 })
+	],
+	controller.updateTechnicianSQL
+);
 router.delete('/sql/:id', controller.deleteTechnicianSQL);
 
 module.exports = router;
